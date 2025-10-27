e4166808e9e009a44cdf2a950505b5f773259cb9
naiferrosado <240394613+naiferrosado@users.noreply.github.com>
2025-10-27 19:38:15 -0400

    Editar app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
+USER_DB = "postgres"
+USER_PASSWORD = "postgres"
+SERVER_DB = "localhost"
+NAME_DB = "Rentflow"
+FULL_URL_DB = f"postgresql://{USER_DB}:{USER_PASSWORD}@{SERVER_DB}/{NAME_DB}"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = FULL_URL_DB
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
