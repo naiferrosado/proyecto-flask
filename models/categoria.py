8a03f723da48d60e626884b4174cc6a7e6e4e401
naiferrosado <240394613+naiferrosado@users.noreply.github.com>
2025-10-27 19:53:22 -0400

    Crear el modelo categoria

---
+++ models/categoria.py
@@ -0,0 +1,11 @@
+from app import db
+
+
+class Categoria(db.Model):
+    __tablename__ = "categoria"
+
+    id_categoria = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), unique=True, nullable=False)
+    descripcion = db.Column(db.String(100))
+
+    objetos = db.relationship("Objeto", backref="categoria", lazy=True)
