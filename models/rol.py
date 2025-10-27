50c53f4265fa8b8c7e7f3ae5130b0bc91a62a42b
naiferrosado <240394613+naiferrosado@users.noreply.github.com>
2025-10-27 19:49:05 -0400

    Crear el modelo rol

---
+++ models/rol.py
@@ -0,0 +1,13 @@
+from app import db
+
+
+class Rol(db.Model):
+    __tablename__ = "rol"
+
+    id_rol = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(
+        db.String(50), unique=True, nullable=False
+    )  # Ej: "Cliente", "Vendedor", "Administrador"
+    descripcion = db.Column(db.String(100))
+
+    usuarios = db.relationship("Usuario", backref="rol", lazy=True)
