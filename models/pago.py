1cf08e9a1e45c85b685bd991e23113eb7c0d2364
naiferrosado <240394613+naiferrosado@users.noreply.github.com>
2025-10-27 19:56:25 -0400

    Agregar el modelo pago

---
+++ models/pago.py
@@ -0,0 +1,14 @@
+from app import db
+
+
+class Pago(db.Model):
+    __tablename__ = "pago"
+
+    id_pago = db.Column(db.Integer, primary_key=True)
+    monto = db.Column(db.Numeric(10, 2), nullable=False)
+    fecha_pago = db.Column(db.Date, nullable=False)
+    metodo = db.Column(db.String(30), nullable=False)
+    estado = db.Column(db.String(20), nullable=False, default="Completado")
+    id_reserva = db.Column(
+        db.Integer, db.ForeignKey("reserva.id_reserva"), nullable=False
+    )
