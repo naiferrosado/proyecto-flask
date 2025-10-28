16f857d0979aebfc987a51aecde93d7aef25dc98
naiferrosado <240394613+naiferrosado@users.noreply.github.com>
2025-10-27 23:10:45 -0400

    Crear migrations y editar rol

--- models/rol.py
+++ models/rol.py
@@ -5,9 +5,7 @@ class Rol(db.Model):
     __tablename__ = "rol"
 
     id_rol = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(
-        db.String(50), unique=True, nullable=False
-    )  # Ej: "Cliente", "Vendedor", "Administrador"
+    nombre = db.Column(db.String(50), unique=True, nullable=False)
     descripcion = db.Column(db.String(100))
 
     usuarios = db.relationship("Usuario", backref="rol", lazy=True)
