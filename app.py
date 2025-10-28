2afba75d3368281a8c7d2d674aa296980f597634
naiferrosado <240394613+naiferrosado@users.noreply.github.com>
2025-10-27 21:01:39 -0400

    Editar app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from extensions import db, migrate  # Importamos desde extensions
+from models import usuario, rol, reserva, pago, opinion, objeto, incidencia, categoria
 
 app = Flask(__name__)
 
@@ -10,11 +11,12 @@ NAME_DB = "Rentflow"
 FULL_URL_DB = f"postgresql://{USER_DB}:{USER_PASSWORD}@{SERVER_DB}/{NAME_DB}"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = FULL_URL_DB
-
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# Inicializamos las extensiones con la app
+db.init_app(app)
+migrate.init_app(app, db)
 
-db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
